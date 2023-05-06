07e1442c9f918d27da45f9745e9c9a65d71bd759
ford <1334788676@qq.com>
2023-05-06 14:10:56 +0800

    修改部署编译选项2

--- src/i18n/language.ts
+++ src/i18n/language.ts
@@ -4,8 +4,15 @@ import { getSearchObj } from '@/helpers/location';
  * 获取语言
  */
 export function getLanguage(): string {
-  const query = getSearchObj();
-  const lang = (query.lang as string) || navigator.language || 'zh-CN';
+  // const query = getSearchObj();
+  // const lang = (query.lang as string) || navigator.language || 'zh-CN';
+  // typeof document !== 'undefined' && document.body.setAttribute('lang', lang);
+  // return lang;
+  let lang = 'zh-CN';
+  if (typeof window !== 'undefined') {
+    const query = getSearchObj();
+    lang = (query.lang as string) || navigator.language || 'zh-CN';
+  }
   typeof document !== 'undefined' && document.body.setAttribute('lang', lang);
   return lang;
 }
